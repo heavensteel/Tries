fe455113c9754b14e252c7e157c35b2cf6e24fdc
heavensteel <secretlessons@gmail.com>
2016-12-23 10:18:18 +0300

    VideoGram

---
+++ Videogram/app/src/main/java/com/seosuz/heaven/videogram/MainActivity.java
@@ -0,0 +1,13 @@
+package com.seosuz.heaven.videogram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
