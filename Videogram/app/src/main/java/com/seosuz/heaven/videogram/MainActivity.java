c1c497e20d4f33169cfaac6dad131d3294033a63
heavensteel <secretlessons@gmail.com>
2016-12-23 12:30:53 +0300

    Little Changes

--- Videogram/app/src/main/java/com/seosuz/heaven/videogram/MainActivity.java
+++ Videogram/app/src/main/java/com/seosuz/heaven/videogram/MainActivity.java
@@ -1,9 +1,15 @@
 package com.seosuz.heaven.videogram;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.net.Uri;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 public class MainActivity extends AppCompatActivity {
 
@@ -12,15 +18,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         try{
-
-            VideoView vidView = (VideoView)findViewById(R.id.myVideo);
+            final Context c = MainActivity.this;
+            final VideoView vidView = (VideoView)findViewById(R.id.myVideo);
             String vidAddress = "http://seosuz.com/alideneme/123.mp4"; //mp4,3gp,webm
+
             Uri vidUri = Uri.parse(vidAddress);
             vidView.setVideoURI(vidUri);
-            vidView.start();
-            MediaController vidControl = new MediaController(this);
-            vidControl.setAnchorView(vidView);
-            vidView.setMediaController(vidControl);
+            vidView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    Toast.makeText(getApplicationContext(),"Videoya tıkladın",Toast.LENGTH_SHORT).toString();
+                    try{
+                            vidView.start();
+                            MediaController vidControl = new MediaController(c);
+                            vidControl.setAnchorView(vidView);
+                            vidView.setMediaController(vidControl);
+
+                    }catch (Exception e){
+                        e.toString();
+                        Log.d("asd",e.getMessage());
+                    }
+                    return false;
+                }
+            });
+
+
         }catch (Exception e){
 
         }
