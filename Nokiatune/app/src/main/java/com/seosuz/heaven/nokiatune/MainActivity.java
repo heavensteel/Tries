5bc5af86b3dbe670a94eb84b72856ad954bc9c31
heavensteel <secretlessons@gmail.com>
2016-12-23 10:14:02 +0300

    Nokia-Tune

--- Nokiatune/app/src/main/java/com/seosuz/heaven/nokiatune/MainActivity.java
+++ Nokiatune/app/src/main/java/com/seosuz/heaven/nokiatune/MainActivity.java
@@ -1,15 +1,22 @@
-package com.seosuz.heaven.tries;
+package com.seosuz.heaven.nokiatune;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Bu bir "," denemedir");
-        System.out.print("Deneee");
+        // #87b703
+        try{
+        }catch (Exception e){
+            e.toString();
+            Log.d("Hata : ", e.getMessage());
+
+        }
+
     }
 }
