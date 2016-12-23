9d1858ad9b5592f9c35354d588d783dae327867d
heavensteel <secretlessons@gmail.com>
2016-12-23 11:24:01 +0300

    Degisiklikller

--- Videogram/app/src/main/java/com/seosuz/heaven/videogram/MainActivity.java
+++ Videogram/app/src/main/java/com/seosuz/heaven/videogram/MainActivity.java
@@ -2,12 +2,27 @@ package com.seosuz.heaven.videogram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.net.Uri;
+import android.widget.MediaController;
+import android.widget.VideoView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try{
+
+            VideoView vidView = (VideoView)findViewById(R.id.myVideo);
+            String vidAddress = "http://seosuz.com/alideneme/123.mp4"; //mp4,3gp,webm
+            Uri vidUri = Uri.parse(vidAddress);
+            vidView.setVideoURI(vidUri);
+            vidView.start();
+            MediaController vidControl = new MediaController(this);
+            vidControl.setAnchorView(vidView);
+            vidView.setMediaController(vidControl);
+        }catch (Exception e){
+
+        }
     }
 }
